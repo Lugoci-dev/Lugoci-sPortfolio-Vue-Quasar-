ca2a5da5ce2e644cc66350c23a64bc338e3eae78
Isaac Daniel <isaitodaiel@gmail.com>
2026-06-06 01:47:35 -0400

    refactor(projects): load projects data from JSON API instead of hardcoded
    - Replace hardcoded project objects in the Pinia store with API fetch
    - Add apiProducts axios instance pointing to data.json
    - Add public/data.json and src/assets/data.json as data sources
    - Update projectBox layout for responsive grid (flex-basis breakpoints)
    - Improve card styling (rounded-xl, spacing)

--- src/components/common/projectBox.vue
+++ src/components/common/projectBox.vue
@@ -18,14 +18,14 @@ const props = defineProps({
 
 <template>
   <div
-    class="flex flex-1 hover:bg-negative gap-2 items-center bg-dark border rounded-md border-Neutral700 p-2 overflow-hidden"
+    class="flex flex-1 md:basis-[calc(50%-1rem)] lg:basis-[calc(33.333%-1rem)] hover:bg-negative gap-2 items-center bg-dark border rounded-xl border-Neutral700 overflow-hidden"
   >
     <q-img
       alt="preview"
       :src="image"
-      class="transition-transform duration-300 ease-in-out not-hover:rounded-lg hover:scale-110"
+      class="transition-transform duration-300 ease-in-out hover:scale-110"
     ></q-img>
-    <div class="flex flex-col gap-2 pb-4">
+    <div class="flex m-2 flex-col gap-2 pb-4">
       <div class="pt-4 flex items-center justify-between">
         <span class="text-weight-bolder text-2xl"> {{ $t(props.name) }}</span>
         <q-icon size="1.5rem" name="more_vert"></q-icon>
--- src/stores/projects-store.js
+++ src/stores/projects-store.js
@@ -1,42 +1,9 @@
 import { defineStore } from 'pinia'
-
-import preview1 from 'src/assets/preview.jpg'
-import preview2 from 'src/assets/preview 2.jpg'
+import { apiProducts } from 'src/boot/axios'
 
 export const useProjectsStore = defineStore('project', {
   state: () => ({
-    projects: [
-      {
-        id: '2ew-wdfFs',
-        name: 'Tesis project',
-        description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit, facere odit nemo? Magnam, repellendus?',
-        relevant: true,
-        imageTop: preview1,
-        languages: ['HTML', 'CSS', 'JavaScript', 'Vue'],
-        tools: ['vue', 'Quasar', 'Stripe'],
-      },
-      {
-        id: '3ew-ydfFs',
-        name: 'Countries Api',
-        description:
-          'Amet nisi inventore iste quo neque aperiam suscipit minus ducimus esse odio debitis obcaecati eum delectus consequuntur.',
-        relevant: true,
-        imageTop: preview2,
-        languages: ['HTML', 'CSS', 'TypeScript', 'Angular', 'API'],
-        tools: ['Angular', 'API'],
-      },
-      {
-        id: '4ew-xdfFs',
-        name: 'Jobs listing',
-        description:
-          'Amet nisi inventore iste quo neque aperiam suscipit minus ducimus esse odio debitis obcaecati eum delectus consequuntur.',
-        relevant: true,
-        imageTop: preview2,
-        languages: ['HTML', 'CSS', 'JavaScript'],
-        tools: ['Json'],
-      },
-    ],
+    projects: [],
   }),
 
   getters: {
@@ -46,8 +13,17 @@ export const useProjectsStore = defineStore('project', {
   },
 
   actions: {
-    increment() {
-      this.counter++
+    async loadData() {
+      try {
+        const response = await apiProducts.get('')
+        // const data = await response.json()
+
+        console.log(response.data)
+        this.projects = response.data
+      } catch (error) {
+        console.warn(error)
+        alert('error')
+      }
     },
   },
 })
